2c843121b0941c5ac72ea3d062a90df6e9e6c43d
seung hwan <developeryunsh@gmail.com>
2023-12-05 21:03:01 +0900

    operator class 생성자 코드 줄이기

--- app/src/main/java/com/example/calculator/calculator/Operator.kt
+++ app/src/main/java/com/example/calculator/calculator/Operator.kt
@@ -1,11 +1,6 @@
 package com.example.calculator.calculator
 
-class Operator(operator: AbstractOperation)  {
-    private var operator: AbstractOperation
-
-    init {
-        this.operator = operator
-    }
+class Operator(private var operator: AbstractOperation)  {
 
     fun operate(num1:Number,num2:Number):Number {
         return operator.operate(num1,num2)
